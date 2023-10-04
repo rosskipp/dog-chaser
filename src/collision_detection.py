d210504915a5b66d7a2530bcd7240cb48a337e7d
Ross Kippenbrock <rkippenbrock@gmail.com>
2023-10-04 07:27:23 -0600

    add object detection code

--- src/collision_detection.py
+++ src/collision_detection.py
@@ -4,11 +4,59 @@ import cv2
 import depthai as dai
 import math
 import numpy as np
+import blobconverter
 import rospy
+import time
 from cv_bridge import CvBridge
 from sensor_msgs.msg import Image
 from std_msgs.msg import Bool, Float32
 
+###
+### User Config
+###
+
+# User-defined constants for collision detection
+WARNING = 1000  # 1m, orange
+CRITICAL = 500  # 50cm, red
+
+fullFrameTracking = True
+
+# NN model
+# model_path = blobconverter.from_zoo(
+#     name="yolov7tiny_coco_640x352", zoo_type="depthai", shaves=6
+# )
+model_path = blobconverter.from_zoo(name="mobilenet-ssd", shaves=5)
+
+###
+### Label Map for MobileNetSSD
+###
+labelMap = [
+    "background",
+    "aeroplane",
+    "bicycle",
+    "bird",
+    "boat",
+    "bottle",
+    "bus",
+    "car",
+    "cat",
+    "chair",
+    "cow",
+    "diningtable",
+    "dog",
+    "horse",
+    "motorbike",
+    "person",
+    "pottedplant",
+    "sheep",
+    "sofa",
+    "train",
+    "tvmonitor",
+]
+
+###
+### ROS SETUP
+###
 
 # CV bridge
 bridge = CvBridge()
@@ -17,8 +65,8 @@ bridge = CvBridge()
 rospy.init_node("depthai_node")
 
 # Create ROS publishers
-imagePub = rospy.Publisher("/object_tracker/image", Image, queue_size=1)
-imagePub = rospy.Publisher("/collision_detection/depth_image", Image, queue_size=1)
+imageObjectPub = rospy.Publisher("/object_tracker/image", Image, queue_size=1)
+imageCollisionPub = rospy.Publisher("/collision_detection/depth_image", Image, queue_size=1)
 leftBoolPub = rospy.Publisher("/collision_detection/left_collision", Bool, queue_size=1)
 leftDistancePub = rospy.Publisher(
     "/collision_detection/left_distance", Float32, queue_size=1
@@ -36,28 +84,41 @@ centerDistancePub = rospy.Publisher(
     "/collision_detection/center_distance", Float32, queue_size=1
 )
 
-# User-defined constants for collision detection
-WARNING = 1000  # 1m, orange
-CRITICAL = 500  # 50cm, red
+###
+### DepthAI Setup
+###
 
 # Create pipeline
 pipeline = dai.Pipeline()
 
 # Define sources and outputs
+camRgb = pipeline.create(dai.node.ColorCamera)
 monoLeft = pipeline.create(dai.node.MonoCamera)
 monoRight = pipeline.create(dai.node.MonoCamera)
 stereo = pipeline.create(dai.node.StereoDepth)
 spatialLocationCalculator = pipeline.create(dai.node.SpatialLocationCalculator)
+objectTracker = pipeline.create(dai.node.ObjectTracker)
+spatialDetectionNetwork = pipeline.create(dai.node.MobileNetSpatialDetectionNetwork)
 
 xoutDepth = pipeline.create(dai.node.XLinkOut)
 xoutSpatialData = pipeline.create(dai.node.XLinkOut)
 xinSpatialCalcConfig = pipeline.create(dai.node.XLinkIn)
+xoutRgb = pipeline.create(dai.node.XLinkOut)
+trackerOut = pipeline.create(dai.node.XLinkOut)
 
 xoutDepth.setStreamName("depth")
 xoutSpatialData.setStreamName("spatialData")
 xinSpatialCalcConfig.setStreamName("spatialCalcConfig")
+xoutRgb.setStreamName("preview")
+trackerOut.setStreamName("tracklets")
 
 ### Properties
+
+# RGB Camera
+camRgb.setPreviewSize(300, 300)
+camRgb.setResolution(dai.ColorCameraProperties.SensorResolution.THE_1080_P)
+camRgb.setInterleaved(False)
+camRgb.setColorOrder(dai.ColorCameraProperties.ColorOrder.BGR)
 
 # Mono
 monoLeft.setResolution(dai.MonoCameraProperties.SensorResolution.THE_720_P)
@@ -75,7 +136,22 @@ stereo.setConfidenceThreshold(50)
 stereo.setExtendedDisparity(True)
 stereo.setSubpixel(True)
 spatialLocationCalculator.inputConfig.setWaitForMessage(False)
+# Align depth map to the perspective of RGB camera, on which inference is done
+stereo.setDepthAlign(dai.CameraBoardSocket.CAM_A)
+stereo.setOutputSize(monoLeft.getResolutionWidth(), monoLeft.getResolutionHeight())
 
+spatialDetectionNetwork.setBlobPath(model_path)
+spatialDetectionNetwork.setConfidenceThreshold(0.5)
+spatialDetectionNetwork.input.setBlocking(False)
+spatialDetectionNetwork.setBoundingBoxScaleFactor(0.5)
+spatialDetectionNetwork.setDepthLowerThreshold(100)
+spatialDetectionNetwork.setDepthUpperThreshold(5000)
+
+objectTracker.setDetectionLabelsToTrack([15])  # track only person
+# possible tracking types: ZERO_TERM_COLOR_HISTOGRAM, ZERO_TERM_IMAGELESS, SHORT_TERM_IMAGELESS, SHORT_TERM_KCF
+objectTracker.setTrackerType(dai.TrackerType.ZERO_TERM_COLOR_HISTOGRAM)
+# take the smallest ID when new object is tracked, possible options: SMALLEST_ID, UNIQUE_ID
+objectTracker.setTrackerIdAssignmentPolicy(dai.TrackerIdAssignmentPolicy.SMALLEST_ID)
 
 # Create rows and columns of ROIs for the SpatialLocationCalculator
 for x in range(15):
@@ -99,6 +175,23 @@ stereo.depth.link(spatialLocationCalculator.inputDepth)
 spatialLocationCalculator.out.link(xoutSpatialData.input)
 xinSpatialCalcConfig.out.link(spatialLocationCalculator.inputConfig)
 
+camRgb.preview.link(spatialDetectionNetwork.input)
+objectTracker.passthroughTrackerFrame.link(xoutRgb.input)
+objectTracker.out.link(trackerOut.input)
+
+if fullFrameTracking:
+    camRgb.setPreviewKeepAspectRatio(False)
+    camRgb.video.link(objectTracker.inputTrackerFrame)
+    objectTracker.inputTrackerFrame.setBlocking(False)
+    # do not block the pipeline if it's too slow on full frame
+    objectTracker.inputTrackerFrame.setQueueSize(2)
+else:
+    spatialDetectionNetwork.passthrough.link(objectTracker.inputTrackerFrame)
+
+spatialDetectionNetwork.passthrough.link(objectTracker.inputDetectionFrame)
+spatialDetectionNetwork.out.link(objectTracker.inputDetections)
+stereo.depth.link(spatialDetectionNetwork.inputDepth)
+
 # Connect to device and start pipeline
 with dai.Device(pipeline) as device:
     device.setIrLaserDotProjectorBrightness(1000)
@@ -108,10 +201,21 @@ with dai.Device(pipeline) as device:
     spatialCalcQueue = device.getOutputQueue(
         name="spatialData", maxSize=4, blocking=False
     )
+
+    preview = device.getOutputQueue("preview", 4, False)
+    tracklets = device.getOutputQueue("tracklets", 4, False)
+
+    startTime = time.monotonic()
+    counter = 0
+    fps = 0
     color = (0, 200, 40)
     fontType = cv2.FONT_HERSHEY_TRIPLEX
 
     while True:
+
+        ###
+        ### Collision Detection Logic
+        ###
         inDepth = (
             depthQueue.get()
         )  # Blocking call, will wait until a new data has arrived
@@ -212,11 +316,112 @@ with dai.Device(pipeline) as device:
                     color,
                 )
 
-        # Show the frame
-        # cv2.imshow("depth", depthFrameColor)
-        # Send the frame
+        ###
+        ### End Collision Detection Logic
+        ###
+
+
+        ###
+        ### Object Detection Logic
+        ###
+
+        imgFrame = preview.get()
+        track = tracklets.get()
+
+        counter += 1
+        current_time = time.monotonic()
+        if (current_time - startTime) > 1:
+            fps = counter / (current_time - startTime)
+            counter = 0
+            startTime = current_time
+
+        objFrame = imgFrame.getCvFrame()
+        trackletsData = track.tracklets
+
+        for t in trackletsData:
+            roi = t.roi.denormalize(objFrame.shape[1], objFrame.shape[0])
+            x1 = int(roi.topLeft().x)
+            y1 = int(roi.topLeft().y)
+            x2 = int(roi.bottomRight().x)
+            y2 = int(roi.bottomRight().y)
+
+            try:
+                label = labelMap[t.label]
+            except:
+                label = t.label
+
+            cv2.putText(
+                objFrame,
+                str(label),
+                (x1 + 10, y1 + 20),
+                cv2.FONT_HERSHEY_TRIPLEX,
+                0.5,
+                255,
+            )
+            cv2.putText(
+                objFrame,
+                f"ID: {[t.id]}",
+                (x1 + 10, y1 + 35),
+                cv2.FONT_HERSHEY_TRIPLEX,
+                0.5,
+                255,
+            )
+            cv2.putText(
+                objFrame,
+                t.status.name,
+                (x1 + 10, y1 + 50),
+                cv2.FONT_HERSHEY_TRIPLEX,
+                0.5,
+                255,
+            )
+            cv2.rectangle(objFrame, (x1, y1), (x2, y2), color, cv2.FONT_HERSHEY_SIMPLEX)
+
+            cv2.putText(
+                objFrame,
+                f"X: {int(t.spatialCoordinates.x)} mm",
+                (x1 + 10, y1 + 65),
+                cv2.FONT_HERSHEY_TRIPLEX,
+                0.5,
+                255,
+            )
+            cv2.putText(
+                objFrame,
+                f"Y: {int(t.spatialCoordinates.y)} mm",
+                (x1 + 10, y1 + 80),
+                cv2.FONT_HERSHEY_TRIPLEX,
+                0.5,
+                255,
+            )
+            cv2.putText(
+                objFrame,
+                f"Z: {int(t.spatialCoordinates.z)} mm",
+                (x1 + 10, y1 + 95),
+                cv2.FONT_HERSHEY_TRIPLEX,
+                0.5,
+                255,
+            )
+
+        cv2.putText(
+            objFrame,
+            "NN fps: {:.2f}".format(fps),
+            (2, frame.shape[0] - 4),
+            cv2.FONT_HERSHEY_TRIPLEX,
+            0.4,
+            color,
+        )
+
+        ###
+        ### End Object Detection Logic
+        ###
+
+
+        # Send the collison frame
         frame = bridge.cv2_to_imgmsg(depthFrameColor, "bgr8")
-        imagePub.publish(frame)
+        imageCollisionPub.publish(frame)
+
+        # send the object detetion frame
+        objSendFrame = bridge.cv2_to_imgmsg(objFrame, "bgr8")
+        imageObjectPub.publish(objSendFrame)
 
         # send the collision messages
         leftBoolPub.publish(leftDetected)
